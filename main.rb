3e7e41102609c9420f480052854c0cfa1e647219
Oscar Mier <voscarmv@gmail.com>
2020-02-27 16:34:37 -0600

    Added my_each_with_index.

--- main.rb
+++ main.rb
@@ -9,8 +9,21 @@ module Enumerable
       self.to_enum
     end
   end
+
+  def my_each_with_index
+    if block_given?
+      for i in 0..self.length-1 do
+        yield(self[i],i)
+      end
+    else
+      self.to_enum
+    end
+  end
 end
 
 [1, 2, 3].my_each
-[4, 5, 6].my_each {|x| puts x+2}
+[4, 5, 6].my_each! {|x| puts x+2}
+
+[7, 9, 10].my_each_with_index {|x| puts "#{x*x}"}
+[7, 9, 10].my_each_with_index {|x,y| puts "#{x+y} #{y}"}
 
