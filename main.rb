fc99f07d86909a2b8a4c26187d65430a87346000
Oscar Mier <voscarmv@gmail.com>
2020-02-27 12:56:36 -0600

    Finished my_each.

--- main.rb
+++ main.rb
@@ -1,8 +1,16 @@
 module Enumerable
   def my_each
-    Enumerator.new(self)
+    if block_given?
+      for i in 0..self.length-1 do
+        yield(self[i])
+      end
+      self
+    else
+      self.to_enum
+    end
   end
 end
 
-[1, 2, 3].each
+[1, 2, 3].my_each
+[4, 5, 6].my_each {|x| puts x+2}
 
