2c5ba9fb3ff7b448dba942f8987292490971c4bc
Oscar Mier <voscarmv@gmail.com>
2020-02-27 20:21:45 -0600

    Simply inverted my_all? logic to create my_none?

--- main.rb
+++ main.rb
@@ -32,7 +32,7 @@ module Enumerable
     end
   end
 
-  def my_each_check
+  def my_each_check_all
     for i in 0..self.length-1 do
       return false unless yield(self[i])
     end
@@ -41,13 +41,32 @@ module Enumerable
 
   def my_all?(expr = nil)
     if expr.class == Regexp
-      self.my_each_check {|i| expr === i}
+      self.my_each_check_all {|i| expr === i}
     elsif expr.class == Class
-      self.my_each_check {|i| i.is_a? expr}
+      self.my_each_check_all {|i| i.is_a? expr}
     elsif block_given?
-      self.my_each_check{|i| yield(i)}
+      self.my_each_check_all {|i| yield(i)}
     else
-      self.my_each_check{|i| i == false || i == nil}
+      self.my_each_check_all {|i| i == false || i == nil}
+    end
+  end
+
+  def my_each_check_none
+    for i in 0..self.length-1 do
+      return false if yield(self[i])
+    end
+    true
+  end
+
+  def my_none?(expr = nil)
+    if expr.class == Regexp
+      self.my_each_check_none {|i| expr === i}
+    elsif expr.class == Class
+      self.my_each_check_none {|i| i.is_a? expr}
+    elsif block_given?
+      self.my_each_check_none {|i| yield(i)}
+    else
+      self.my_each_check_none {|i| i == false || i == nil}
     end
   end
 
@@ -70,3 +89,11 @@ end
 [1, 2i, 3.14].my_all?(Numeric) 
 [1, 2, 3].my_all?(Integer)
 [9, "abc", 0].my_all?(Integer)
+
+["oscar", "apple", "dark"].my_none?(/a/)
+["oscar", "apple", "dark"].my_none?(/b/)
+[7, 1, 2, 3, 4, 5].my_none? {|x| x > 9}
+[7, 1, 2, 3, 4, 5].my_none? {|x| x > 0}
+[1, 2i, 3.14].my_none?(Numeric) 
+[1, 2, 3].my_none?(Integer)
+[9, "abc", 0].my_none?(Integer)
