90e11e756fc61b0000dee1869bf92843e404f4b3
Oscar Mier <voscarmv@gmail.com>
2020-02-29 11:24:37 -0600

    Corrections applied.

--- main.rb
+++ main.rb
@@ -3,148 +3,170 @@
 # rubocop:disable Metrics/CyclomaticComplexity
 # rubocop:disable Metrics/PerceivedComplexity
 module Enumerable
-  def my_each
-    if block_given?
-      (0..length - 1).each do |i|
-        yield(self[i])
-      end
-      self
+  def make_array(input)
+    if input.class == Range
+      input.to_a
     else
-      to_enum
+      input
+    end
+  end
+
+  def my_each
+    arr = make_array(self)
+    if block_given?
+      i = 0
+      while i < arr.length
+        yield(arr[i])
+        i += 1
+      end
+    else
+      arr.to_enum
+    end
+  end
+
+  def my_each_2
+    arr = make_array(self)
+    if block_given?
+      i = 1
+      while i < arr.length
+        yield(arr[i])
+        i += 1
+      end
+    else
+      arr.to_enum
     end
   end
 
   def my_each_with_index
+    arr = make_array(self)
     if block_given?
-      (0..length - 1).each do |i|
-        yield(self[i], i)
-      end
+      [0..arr.length - 1].my_each { |i| yield(arr[i], i) }
     else
-      to_enum
+      arr.to_enum
     end
   end
 
   def my_select
+    arr = make_array(self)
     if block_given?
       output = []
-      (0..length - 1).each do |i|
-        output << self[i] if yield(self[i])
-      end
+      arr.my_each { |i| output << i if yield(i) }
       output
     else
-      to_enum
+      arr.to_enum
     end
   end
 
-  def my_each_check_all
-    (0..length - 1).each do |i|
-      return false unless yield(self[i])
-    end
+  def my_all_check
+    arr = make_array(self)
+    arr.my_each { |i| return false unless yield(i) }
+    true
+  end
+
+  def my_any_check
+    arr = make_array(self)
+    arr.my_each { |i| return true if yield(i) }
+    false
+  end
+
+  def my_none_check
+    arr = make_array(self)
+    arr.my_each { |i| return false if yield(i) }
     true
   end
 
   def my_all?(expr = nil)
     if expr.class == Regexp
-      my_each_check_all { |i| i =~ expr }
+      my_all_check { |i| i =~ expr }
     elsif expr.class == Class
-      my_each_check_all { |i| i.is_a? expr }
+      my_all_check { |i| i.is_a? expr }
+    elsif !expr.nil?
+      my_all_check { |i| i == expr }
     elsif block_given?
-      my_each_check_all { |i| yield(i) }
+      my_all_check { |i| yield(i) }
     else
-      my_each_check_all { |i| i == false || i.nil? }
+      my_none_check { |i| i == false || i.nil? }
     end
   end
 
-  def my_each_check_none
-    (0..length - 1).each do |i|
-      return false if yield(self[i])
+  def my_any?(expr = nil)
+    if expr.class == Regexp
+      my_any_check { |i| i =~ expr }
+    elsif expr.class == Class
+      my_any_check { |i| i.is_a? expr }
+    elsif !expr.nil?
+      my_any_check { |i| i == expr }
+    elsif block_given?
+      my_any_check { |i| yield(i) }
+    else
+      my_any_check { |i| i != false && !i.nil? }
     end
-    true
   end
 
   def my_none?(expr = nil)
     if expr.class == Regexp
-      my_each_check_none { |i| i =~ expr }
+      my_none_check { |i| i =~ expr }
     elsif expr.class == Class
-      my_each_check_none { |i| i.is_a? expr }
+      my_none_check { |i| i.is_a? expr }
+    elsif !expr.nil?
+      my_none_check { |i| i == expr }
     elsif block_given?
-      my_each_check_none { |i| yield(i) }
+      my_none_check { |i| yield(i) }
     else
-      my_each_check_none { |i| i == false || i.nil? }
+      my_all_check { |i| i == false || i.nil? }
     end
   end
 
   def my_count(obj = nil)
-    output = 0
+    arr = make_array(self)
+    count = 0
     if !obj.nil?
-      (0..length - 1).each do |i|
-        output += 1 if self[i] == obj
-      end
-      output
+      arr.my_each { |i| count += 1 if i == obj }
+      count
     elsif block_given?
-      (0..length - 1).each do |i|
-        output += 1 if yield(self[i])
-      end
-      output
+      arr.my_each { |i| count += 1 if yield(i) }
+      count
     else
-      length
+      arr.length
     end
   end
 
   def my_map(proc = nil)
+    arr = make_array(self)
     output = []
     if proc.class == Proc
-      (0..length - 1).each do |i|
-        output << proc.call(self[i])
-      end
+      arr.my_each { |i| output << proc.call(i) }
       output
     elsif block_given?
-      (0..length - 1).each do |i|
-        output << yield(self[i])
-      end
+      arr.my_each { |i| output << yield(i) }
       output
     else
-      to_enum
+      arr.to_enum
     end
   end
 
   def my_inject(first = nil, second = nil)
-    accum = self[0]
+    arr = make_array(self)
+    accum = arr[0]
     if first.class == Symbol
-      (1..length - 1).each do |i|
-        accum = accum.send(first, self[i])
-      end
+      arr.my_each_2 { |i| accum = accum.send(first, i) }
       accum
-    elsif !first.class.nil? && second.nil?
-      accum = first
+    elsif !first.nil? && second.nil?
       if block_given?
-        (0..length - 1).each do |i|
-          accum = yield(accum, self[i])
-        end
-      else
-        (0..length - 1).each do |i|
-          accum = accum.send(first, self[i])
-        end
+        accum = first
+        arr.my_each { |i| accum = yield(accum, i) }
+        accum
       end
-      accum
-    elsif !first.class.nil? && second.class == Symbol
+    elsif !first.nil? && second.class == Symbol
       accum = first
-      (0..length - 1).each do |i|
-        accum = accum.send(second, self[i])
-      end
+      arr.my_each { |i| accum = accum.send(second, i) }
       accum
     elsif first.nil? && second.nil?
       if block_given?
-        (1..length - 1).each do |i|
-          accum = yield(accum, self[i])
-        end
+        arr.my_each_2 { |i| accum = yield(accum, i) }
         accum
       end
     end
-  end
-
-  def multiply_els
-    my_inject(:*)
   end
 end
 # rubocop:enable Metrics/ModuleLength
@@ -152,87 +174,49 @@ end
 # rubocop:enable Metrics/CyclomaticComplexity
 # rubocop:enable Metrics/PerceivedComplexity
 
-[1, 2, 3].my_each
-[4, 5, 6].my_each { |x| puts x + 2 }
+def multiply_els(input)
+  input.my_inject(:*)
+end
 
-[1, 2, 3].each
-[4, 5, 6].each { |x| puts x + 2 }
+[1, 2, 3, 4].my_each { |x| puts "number: #{x}" }
 
-[7, 9, 10].my_each_with_index
-[7, 9, 10].my_each_with_index { |x| puts(x * x).to_s }
-[7, 9, 10].my_each_with_index { |x, y| puts "#{x + y} #{y}" }
+[1, 2, 3, 4].my_each_with_index { |x, y| puts "number #{y}: #{x}" }
 
-[7, 9, 10].each_with_index
-[7, 9, 10].each { |x| puts(x * x).to_s }
-[7, 9, 10].each_with_index { |x, y| puts "#{x + y} #{y}" }
+[1, 2, 3, 4].my_select { |i| i > 1 }
 
-[0, 0, 0, 1, 0, 2].my_select
-[0, 0, 0, 1, 0, 2].my_select(&:positive?)
-
-[0, 0, 0, 1, 0, 2].select
-[0, 0, 0, 1, 0, 2].select(&:positive?)
-
+[0, 0, 0, 1, 2, 0].my_all? { |i| i < 5 }
 %w[oscar apple dark].my_all?(/a/)
-%w[oscar apple dark].my_all?(/b/)
-[7, 1, 2, 3, 4, 5].my_all? { |x| x > 9 }
-[7, 1, 2, 3, 4, 5].my_all?(&:positive?)
 [1, 2i, 3.14].my_all?(Numeric)
-[1, 2, 3].my_all?(Integer)
-[9, 'abc', 0].my_all?(Integer)
 
-%w[oscar apple dark].all?(/a/)
-%w[oscar apple dark].all?(/b/)
-[7, 1, 2, 3, 4, 5].all? { |x| x > 9 }
-[7, 1, 2, 3, 4, 5].all?(&:positive?)
-[1, 2i, 3.14].all?(Numeric)
-[1, 2, 3].all?(Integer)
-[9, 'abc', 0].all?(Integer)
+%w[ant bear cat].my_none? { |word| word.length >= 4 }
+%w[ant bear cat].my_none?(/d/)
+[1, 3.14, 42].my_none?(Float)
 
-%w[oscar apple dark].my_none?(/a/)
-%w[oscar apple dark].my_none?(/b/)
-[7, 1, 2, 3, 4, 5].my_none? { |x| x > 9 }
-[7, 1, 2, 3, 4, 5].my_none?(&:positive?)
-[1, 2i, 3.14].my_none?(Numeric)
-[1, 2, 3].my_none?(Integer)
-[9, 'abc', 0].my_none?(Integer)
+ary = [1, 2, 4, 2]
+ary.my_count #=> 4
+ary.my_count(2) #=> 2
+ary.my_count(&:even?) #=> 3
 
-%w[oscar apple dark].none?(/a/)
-%w[oscar apple dark].none?(/b/)
-[7, 1, 2, 3, 4, 5].none? { |x| x > 9 }
-[7, 1, 2, 3, 4, 5].none?(&:positive?)
-[1, 2i, 3.14].none?(Numeric)
-[1, 2, 3].none?(Integer)
-[9, 'abc', 0].none?(Integer)
+(1..4).map { |i| i * i } #=> [1, 4, 9, 16]
+(1..4).collect { 'cat' } #=> ["cat", "cat", "cat", "cat"]
 
-[0, 2, 3, 4, 5, 1, 2, 2, 3].my_count(1)
-[0, 2, 3, 4, 5, 1, 2, 2, 3].my_count(2)
-[0, 2, 3, 4, 5, 1, 2, 2, 3].my_count(3)
-[0, 2, 3, 4, 5, 1, 2, 2, 3].my_count { |i| i > 2 }
-[0, 2, 3, 4, 5, 1, 2, 2, 3].my_count { |i| i < 2 }
+# Sum some numbers
+(5..10).my_inject(:+) #=> 45
+# Same using a block and inject
+(5..10).my_inject { |sum, n| sum + n } #=> 45
+# Multiply some numbers
+(5..10).my_inject(1, :*) #=> 151200
+# Same using a block
+(5..10).my_inject(1) { |product, n| product * n } #=> 151200
+# find the longest word
+longest = %w[cat sheep bear].my_inject do |memo, word|
+  memo.length > word.length ? memo : word
+end
+puts longest #=> "sheep"
 
-[0, 2, 3, 4, 5, 1, 2, 2, 3].count(1)
-[0, 2, 3, 4, 5, 1, 2, 2, 3].count(2)
-[0, 2, 3, 4, 5, 1, 2, 2, 3].count(3)
-[0, 2, 3, 4, 5, 1, 2, 2, 3].count { |i| i > 2 }
-[0, 2, 3, 4, 5, 1, 2, 2, 3].count { |i| i < 2 }
+multiply_els([2, 4, 5])
 
-[0, 0, 0, 1, 0, 2].my_map
-[0, 0, 0, 1, 0, 2].my_map(&:positive?)
-myproc = proc { |i| i > 1 }
+myproc = proc { |i| i > 2 }
 [0, 0, 0, 1, 0, 2, 3, 4].my_map(myproc)
 
-[0, 0, 0, 1, 0, 2].map
-[0, 0, 0, 1, 0, 2].map(&:positive?)
-# myproc = proc { |i| i > 1 }
-# [0, 0, 0, 1, 0, 2, 3, 4].map(myproc)
-
-[1, 2, 3, 4, 5].my_inject(:+)
-[1, 2, 3, 4, 5].my_inject(10, :+)
-[1, 2, 3, 4, 5].my_inject(10) { |x, y| x + y }
-
-[1, 2, 3, 4, 5].inject(:+)
-[1, 2, 3, 4, 5].inject(10, :+)
-[1, 2, 3, 4, 5].inject(10) { |x, y| x + y }
-
-[2, 4, 5].multiply_els
-[2, 4, 5].inject(:*)
+[false, false, nil].my_any?
